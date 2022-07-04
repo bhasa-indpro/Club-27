7d2028d3f89a2b25ca9d225b33431a7f875b02ec
bhasa-indpro <aronyanil@indpro.se>
2022-07-04 09:08:07 +0530

    Test Buttons Added - Click to change colour, hover to change colour, click to open text box

---
+++ Club 27/Controllers/MockTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Club_27.Controllers
+{
+    public class MockTestController : Controller
+    {
+        public IActionResult ButtonActions()
+        {
+            return View();
+        }
+    }
+}
